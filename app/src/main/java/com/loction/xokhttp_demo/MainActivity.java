974a9c0f5bf78932d08bafbf31d769bc3091e2c8
localadmin <920847827@qq.com>
2017-10-27 13:17:10 +0800

    1

--- app/src/main/java/com/loction/xokhttp_demo/MainActivity.java
+++ app/src/main/java/com/loction/xokhttp_demo/MainActivity.java
@@ -3,11 +3,14 @@ package com.loction.xokhttp_demo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.loction.xokhttp.XOkhttpClient;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
